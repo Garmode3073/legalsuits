b8094ad8c0fffa1e5484e0904185d57bdde7b975
Garmode3073 <gauravgarmode16@gmail.com>
2021-12-27 13:03:49 +0530

    setup globals.dart

---
+++ lib/globals.dart
@@ -0,0 +1,21 @@
+//measurements
+import 'package:flutter/material.dart';
+
+double width, height;
+
+//color swatch
+Map<int, Color> color = {
+  50: Color.fromRGBO(211, 155, 45, .1),
+  100: Color.fromRGBO(211, 155, 45, .2),
+  200: Color.fromRGBO(211, 155, 45, .3),
+  300: Color.fromRGBO(211, 155, 45, .4),
+  400: Color.fromRGBO(211, 155, 45, .5),
+  500: Color.fromRGBO(211, 155, 45, .6),
+  600: Color.fromRGBO(211, 155, 45, .7),
+  700: Color.fromRGBO(211, 155, 45, .8),
+  800: Color.fromRGBO(211, 155, 45, .9),
+  900: Color.fromRGBO(211, 155, 45, 1),
+};
+
+//colors
+Color primary = MaterialColor(0xffD39B2D, color);
--- lib/main.dart
+++ lib/main.dart
@@ -14,38 +14,6 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-          height: 500,
-          width: 500,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: NetworkImage(
-                'https://cdn.mos.cms.futurecdn.net/9R7qcYvf74sqdXzVGAUtCJ-1024-80.jpg.webp',
-              ),
-              fit: BoxFit.contain,
-            ),
-          ),
-        ),
-      ),
     );
   }
 }
