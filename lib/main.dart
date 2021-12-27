e7b4f8b14b14ea7151a7dd21ebc9eefe01f8399e
Garmode3073 <gauravgarmode16@gmail.com>
2021-12-27 13:07:26 +0530

    add loading screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:provider/provider.dart';
+import 'package:legalsuits/globals.dart' as g;
+import 'package:legalsuits/screens/loading.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,10 +10,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      title: 'Legal Suites',
+      theme: ThemeData(primarySwatch: g.primary),
+      home: LoadingPage(),
     );
   }
 }
